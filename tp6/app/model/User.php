2043dc47029c7edc430eb76fa8f59b02b23a1428
unknown <jh.zeng@mtsnet.com>
2021-09-28 12:37:50 +0800

    增加模型远程一对多的使用

--- tp6/app/model/User.php
+++ tp6/app/model/User.php
@@ -16,6 +16,17 @@ class User extends Model
         return $this->hasMany(Role::class, 'user_id', 'id');
     }
 
+    /**
+     * 远程一对多，通过中间表查询，适合RBAC等
+     *
+     * @return void
+     */
+    public function roles2()
+    {
+        //SELECT `role`.* FROM `tp_role` `role` INNER JOIN `tp_user_role` ON `tp_user_role`.`role_id`=`role`.`id`
+        // INNER JOIN `tp_user` ON `tp_user`.`id`=`tp_user_role`.`user_id` WHERE  `tp_user_role`.`user_id` = '2'                                                           
+        return $this->hasManyThrough(Role::class, UserRole::class, 'user_id', 'id', 'id', 'role_id');
+    }
 
     /**
      * user 是主表，info是从表
